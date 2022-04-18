10ae8085f6d9c2ebbbf5ed1e39a72f8f7e68f46c
Nicolas Wolf <haiyang@awolf.net>
2022-04-18 22:53:54 +0800

    formatted document

--- wp-content/themes/linna/template-parts/sidebar/sidebar-top.php
+++ wp-content/themes/linna/template-parts/sidebar/sidebar-top.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the top sidebar area
  *
@@ -9,25 +10,25 @@
 
 ?>
 <div class="site-sidebar-top site-primary-bg sidebar-box">
-	<?php $sidebar_top_logo = linna_option( array( 'sidebar-top-logo', 'url' ) ); ?>
-	<?php $sidebar_top_logo_border_radius = linna_option( 'sidebar-top-logo-border-radius' ); ?>
-	<?php $sidebar_top_text_1 = linna_option( 'sidebar-top-text-1' ); ?>
-	<?php $sidebar_top_text_2 = linna_option( 'sidebar-top-text-2' ); ?>
+	<?php $sidebar_top_logo = linna_option(array('sidebar-top-logo', 'url')); ?>
+	<?php $sidebar_top_logo_border_radius = linna_option('sidebar-top-logo-border-radius'); ?>
+	<?php $sidebar_top_text_1 = linna_option('sidebar-top-text-1'); ?>
+	<?php $sidebar_top_text_2 = linna_option('sidebar-top-text-2'); ?>
 
-	<?php if ( ! empty( $sidebar_top_logo ) ) : ?>
-		<img src="<?php echo esc_attr( $sidebar_top_logo ); ?>" style="
+	<?php if (!empty($sidebar_top_logo)) : ?>
+		<img src="<?php echo esc_attr($sidebar_top_logo); ?>" style="
 		<?php
-		if ( ! empty( $sidebar_top_logo_border_radius ) ) :
-			?>
-				border-radius:<?php echo esc_attr( $sidebar_top_logo_border_radius ); ?>%;<?php endif; ?>" alt="">
+		if (!empty($sidebar_top_logo_border_radius)) :
+		?>
+				border-radius:<?php echo esc_attr($sidebar_top_logo_border_radius); ?>%;<?php endif; ?>" alt="">
 	<?php endif; ?>
 
 	<?php
-	if ( ! empty( $sidebar_top_text_1 ) ) :
-		?>
-		<h3><?php echo esc_html( $sidebar_top_text_1 ); ?></h3><?php endif; ?>
+	if (!empty($sidebar_top_text_1)) :
+	?>
+		<h3><?php echo esc_html($sidebar_top_text_1); ?></h3><?php endif; ?>
 	<?php
-	if ( ! empty( $sidebar_top_text_2 ) ) :
-		?>
-		<h5><?php echo esc_html( $sidebar_top_text_2 ); ?></h5><?php endif; ?>
+	if (!empty($sidebar_top_text_2)) :
+	?>
+		<h5><?php echo esc_html($sidebar_top_text_2); ?></h5><?php endif; ?>
 </div>
